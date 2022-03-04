4f52d582a4f1a754021abb60ac6a28e130d25e60
Seth Kerr <Seth_Kerr@carmax.com>
2022-03-04 12:21:23 -0500

    Add project files.

---
+++ BlazorServer/Pages/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorServer.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private string countColor = "red";
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
